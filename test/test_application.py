7be495736568b74ffd7bf5b2d35f8896dac9e7fc
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2015-12-29 22:51:20 +0200

    Add echo tests

---
+++ test/test_application.py
@@ -0,0 +1,21 @@
+import unittest
+import json
+import application
+
+class ApplicationTestCase(unittest.TestCase):
+
+  def setUp(self):
+    application.app.config['TESTING'] = True
+    self.app = application.app.test_client()
+
+  def test_root(self):
+    response = self.app.get('/')
+    self.assertEqual(response.status_code, 404)
+
+  def test_echoing(self):
+    response = self.app.get('/foo')
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.get_data(), '{"foo": "foo"}')
+
+if __name__ == '__main__':
+  unittest.main()
